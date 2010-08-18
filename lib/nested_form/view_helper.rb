3d63a09149117315f3fda636fb81ef73ce825a91
Jakob Skov-Pedersen <jasp@keepfocus.dk>
2010-08-18 13:56:43 +0200

    Update specs and code for rails 3

--- lib/nested_form/view_helper.rb
+++ lib/nested_form/view_helper.rb
@@ -2,9 +2,9 @@ module NestedForm
   module ViewHelper
     def nested_form_for(*args, &block)
       options = args.extract_options!.reverse_merge(:builder => NestedForm::Builder)
-      form_for(*(args << options), &block) 
+      form_for(*(args << options), &block)
       @after_nested_form_callbacks ||= []
-      @after_nested_form_callbacks.each do |callback|
+      @after_nested_form_callbacks.collect do |callback|
         callback.call
       end
     end
